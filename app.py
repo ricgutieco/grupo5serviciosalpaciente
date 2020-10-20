f7bb8804a5046f93a4e3cfcfe2cb37142862cc66
Alejandro Triana <alejandro183@gmail.com>
2020-10-19 20:15:27 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import configparser as configparser
+from flask import Flask, Response, json
+from database.db import initialize_db
+from flask_restful import Api
+from resources.routes import initialize_routes
+import ConfigParser
+
+app = Flask(__name__)
+api = Api(app)
+
+config = configparser.ConfigParser()
+config.read('config.properties')
+app.config['MONGODB_SETTINGS'] = {
+    'host': config.get(section='database_config', option='host') + config.get(section='database_config', option='database')
+}
+
+
+@app.route('/')
+def base():
+    return Response(response=json.dumps({"Status": "UP", 'db_host': config.get(section='database_config', option='host')}),
+                    status=200,
+                    mimetype='application/json')
+
+initialize_db(app)
+initialize_routes(api)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host='0.0.0.0')
